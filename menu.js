b1e6d060a883581cef45aab11c630c7137f05daf
Jae Huh <jaeeunhuh@gmail.com>
2017-04-01 22:35:31 +1100

    Modify to be compatible with menu.js

--- menu.js
+++ menu.js
@@ -13,6 +13,22 @@ var menu = [
     {
       title: 'HTML, CSS and DOM - Part 2',
       href: '/blog/t2-html-css-dom-p2.html'
+    },
+    {
+      title: 'Design to Web Blog',
+      href: '/blog/t3-design-to-web-blog.html'
+    },
+    {
+      title: 'JavaScript Basics',
+      href: '/blog/t4-javascript-basics.html'
+    },
+    {
+      title: 'Problem Solving',
+      href: '/blog/t5-problem-solving.html'
+    },
+    {
+      title: 'JavaScript Language',
+      href: '/blog/t6-js-language.html'
     }
   ],
   [
@@ -23,16 +39,44 @@ var menu = [
     {
       title: 'Time and Habits',
       href: '/blog/c1-time-and-habits-blog.html'
+    },
+    {
+      title: 'Emotional Intelligence',
+      href: '/blog/c2-emotional-intelligence.html'
+    },
+    {
+      title: 'Meditation Process',
+      href: '/blog/c3-meditation-process.html'
+    },
+    {
+      title: 'Daily Mindfulness',
+      href: '/blog/c4-daily-mindfulness.html'
+    },
+    {
+      title: 'Self-Confidence',
+      href: '/blog/c5-self-confidence.html'
+    },
+    {
+      title: 'Control',
+      href: '/blog/c6-control.html'
     }
   ],
-  // []
+  // [] for projects
 ]
 
 
 function generateMenu() {
+  //link to home
+  document.querySelector('#home-about').innerHTML = '<a href="' + menu[0][0].href + '">' + menu[0][0].title + '</a>'
+
   //lists technical blog
   for (var i = 0; i < menu[1].length; i++) {
     document.querySelector('#technical').innerHTML += '<li><a href="' + menu[1][i].href + '">' + menu[1][i].title + '</a></li>'
+  }
+
+  //lists cultural blog
+  for (var i = 0; i < menu[1].length; i++) {
+    document.querySelector('#cultural').innerHTML += '<li><a href="' + menu[2][i].href + '">' + menu[2][i].title + '</a></li>'
   }
 }
 
